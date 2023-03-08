dcc24cf01a86cfe4ebbb9a7c798ba834febba28b
Lampros <121090005+lamprosKrikelis@users.noreply.github.com>
2023-03-08 15:06:06 +0200

    Add files via upload

---
+++ DijkstraAlgo_RedBlackTrees/DijkstraTreeSet1.java
@@ -0,0 +1,147 @@
+// hybrid Graph . Wrong the  following algorithm does NOT print the correct answer !
+
+
+import javafx.util.Pair;
+import java.lang.management.MemoryManagerMXBean;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.TreeSet;
+ import java.util.Scanner;
+
+
+// Shortest Path Dijkstra algorithm ... 
+
+public class DijkstraTreeSet1
+{
+    static class path {
+        int src ;
+        int dest;
+        int weight;
+
+        public path(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+    static class PairComparator implements Comparator<Pair<Integer, Integer>>{
+
+        @Override
+        public int compare(Pair<Integer, Integer> o1, Pair<Integer, Integer> o2) {
+            //sort using distance values
+            int key1 = o1.getKey();
+            int key2 = o2.getKey();
+            return key1-key2;
+        }
+    }
+
+      
+    static class Graph {
+        int vertices; 
+        LinkedList<path>[] adjacencylist;
+
+        Graph(int vertices) {
+            this.vertices = vertices;
+            adjacencylist = new LinkedList[vertices];
+            //initialize adjacency lists for all the vertices
+            for (int i = 0; i <vertices ; i++) {
+                adjacencylist[i] = new LinkedList<>();
+
+            }
+
+        }
+
+
+        public void addEdge(int src, int dest, int weight) {
+            path path = new path(src, dest, weight);
+            adjacencylist[src].addFirst(path);
+
+            path = new path(dest, src, weight);
+            adjacencylist[dest].addFirst(path);             //for undirected graph
+        }
+
+        public void dijkstra_GetMinDistances(int sourceVertex){
+
+            boolean[]  inSPT = new boolean[vertices];
+            //distance used to store the distance of vertex from a source
+            int [] distance = new int[vertices];
+
+            //Initialize all the distances to infinity
+            for (int i = 0; i <vertices ; i++) {
+                distance[i] = Integer.MAX_VALUE;
+            }
+            //Initialize priority queue
+            //override the comparator to do the sorting based keys
+            TreeSet treeSet = new TreeSet<>(new PairComparator());
+            //create the pair for for the first index, 0 distance 0 index
+            distance[0] = 0;
+            Pair<Integer, Integer> p0 = new Pair<>(distance[0],0);
+            //add it to tree set
+            treeSet.add(p0);
+
+            //while priority queue is not empty
+            while(!treeSet.isEmpty()){
+                //extract the min
+                Pair<Integer, Integer> extractedPair = (Pair<Integer, Integer>) treeSet.pollFirst();
+
+                //extracted vertex
+                int extractedVertex = extractedPair.getValue();
+                if(inSPT[extractedVertex]==false) {
+                    inSPT[extractedVertex] = true;
+
+                    //iterate through all the adjacent vertices and update the keys
+                    LinkedList<path> list = adjacencylist[extractedVertex];
+                    for (int i = 0; i < list.size(); i++) {
+                        path path = list.get(i);
+                        int dest = path.dest;
+                        //only if edge destination is not present 
+                        if (inSPT[dest] == false) {
+                            //check if distance needs an update or not
+                            //means check total weight from source to vertex_V is less than
+                            //the current distance value, if yes then update the distance
+                            int newKey =  distance[extractedVertex] + path.weight ;
+                            int currentKey = distance[dest];
+                            if(currentKey>newKey){
+                                Pair<Integer, Integer> p = new Pair<>(newKey, dest);
+                                treeSet.add(p);
+                                distance[dest] = newKey;
+                            }
+                        }
+                    }
+                }
+            }
+            //print Shortest Path Tree
+            printDijkstra(distance, sourceVertex);                  
+        }
+
+
+        public void printDijkstra(int[] distance, int sourceVertex){
+            for (int i = 0; i <vertices ; i++) {
+                System.out.println("Source Vertex: " + sourceVertex + " to vertex " +   + i +
+                        " distance: " + distance[i]);
+            }
+        } 
+    
+        public static void main(String[] args) {
+            int vertices = 8 ;
+            Graph graph = new Graph(vertices);
+            graph.addEdge(0, 1, 4);
+            graph.addEdge(0, 2, 3);
+            graph.addEdge(1, 2, 1);
+            graph.addEdge(1, 0, 4);
+            graph.addEdge(2, 3, 4);
+            graph.addEdge(3, 4, 1);
+            graph.addEdge(4, 1, 7);
+            graph.addEdge(4, 5, 2);
+            graph.addEdge(5, 4, 2);
+            graph.addEdge(5, 2, 3);
+            graph.addEdge(5, 7, 4);
+            graph.addEdge(6, 5, 6);
+            graph.addEdge(6, 2, 4);
+            graph.addEdge(7, 5, 4);
+            graph.dijkstra_GetMinDistances(0);
+        
+        }
+
+    }
+}
